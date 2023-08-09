49b3af6209849126ac49f82b6c5a8dff15688a32
omnia <omniannnnnnn@gmail.com>
2023-08-09 13:51:03 -0700

    cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,15 +7,15 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *s = list;
-	listint_t *f = list;
+listint_t *s = list;
+listint_t *f = list;
 
-	while (s && f && f->next)
-	{
-		s = s->next;
-		f = f->next->next;
-		if (s == f)
-			return (1);
-	}
-	return (0);
+while (s && f && f->next)
+{
+s = s->next;
+f = f->next->next;
+if (s == f)
+return (1);
+}
+return (0);
 }
