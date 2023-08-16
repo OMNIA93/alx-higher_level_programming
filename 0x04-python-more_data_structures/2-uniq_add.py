a880e14bab69d7f8975f9e2d11375ea25d1ee7b4
omnia <omniannnnnnn@gmail.com>
2023-08-16 14:46:24 +0000

    uniqe

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,4 +4,7 @@ def uniq_add(my_list=[]):
     for item in my_list:
         if item not in new_list:
             new_list.append(item)
-    return new_list
+    total = 0
+    for num in new_list:
+        total += num
+    return total
