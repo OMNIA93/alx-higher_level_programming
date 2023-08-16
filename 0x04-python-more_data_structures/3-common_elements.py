b1180ad116df1601aa1dda8e4af14f3a5158d237
omnia <omniannnnnnn@gmail.com>
2023-08-16 16:11:06 +0000

    bg

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-common_set = set()
+	common_set = set()
     
-for item in set_1:
-if item in set_2:
-common_set.add(item)
+	for item in set_1:
+		if item in set_2
+			common_set.add(item)
     
-return common_set
+    return common_set
