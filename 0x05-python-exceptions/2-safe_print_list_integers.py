8cdf5ba0e550b947e53559344b51aff161898529
omnia <omniannnnnnn@gmail.com>
2023-08-28 20:16:30 +0000

    task2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-printed_list = 0
-for idx in rang(x):
-printed_list += 1
-try
-print "{:d}".format(my_list[idx]), end="")
-except (ValueError, TypeError):
-printed_list -= 1
-print()
-return printed_list
+m = 0
+    for i in range(x):
+        m += 1
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (ValueError, TypeError):
+            m -= 1
+    print()
+    return m
