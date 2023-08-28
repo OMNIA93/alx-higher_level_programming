67a15703eac81a9a17a78631dd28733ad5cf29d2
omnia <omniannnnnnn@gmail.com>
2023-08-28 14:37:12 +0000

    task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
-count = 0
-try:
-for i in range(x):
-print(my_list[i], end='')
-count += 1
-except IndexError:
-pass
-print()
-return count
+    printed_elements = 0
+
+    for idx in range(x):
+        try:
+            print('{}'.format(my_list[idx]), end="")
+            printed_elements += 1
+        except IndexError:
+            break
+
+    print("")
+    return printed_elements
