43ee70ed9923c8d49ef3e908ec991d3b5342d1ed
omnia <omniannnnnnn@gmail.com>
2023-08-28 14:39:54 +0000

    test0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,14 +2,14 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    printed_elements = 0
+    print_length_elements = 0
 
     for idx in range(x):
         try:
             print('{}'.format(my_list[idx]), end="")
-            printed_elements += 1
+            print_length_elements += 1
         except IndexError:
-            break
+            pass
 
     print("")
-    return printed_elements
+    return print_length_elements
