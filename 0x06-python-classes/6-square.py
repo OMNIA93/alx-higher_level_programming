f20ff3547778618914b05dd7eb6b0f1d0182a51c
omnia <omniannnnnnn@gmail.com>
2023-08-29 20:48:39 +0000

    task6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" define a Square """
+
+
+class Square:
+    """my Square"""
+    def __init__(self, size=0, position=(0, 0)):
+        """init"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Size Geter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Size Seter"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """position"""
+        if not isinstance(value, tuple) or len(value) != 2 or\
+                not all(isinstance(num, int) and num >= 0 for num in value):
+            raise TypeError("position must be a \
+tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        """my_print"""
+        for i in range(self.__position[1]):
+            print()
+        if not self.__size:
+            print()
+            return
+        for i in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
