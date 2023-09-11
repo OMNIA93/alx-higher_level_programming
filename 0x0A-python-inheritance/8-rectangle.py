71a09ddf3e3877283ac7c413bdb15aa411392169
omnia <omniannnnnnn@gmail.com>
2023-09-11 21:57:39 +0000

    again

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,25 +1,13 @@
 #!/usr/bin/python3
-"""
-Contains the BaseGeometry and Rectangle classes
-"""
+"""BaseGeometry class"""
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """A subclass of BaseGeometry that represents a rectangle"""
-
+    """BaseGeometry Calss"""
     def __init__(self, width, height):
-        """Initializes a Rectangle instance
-
-        Args:
-            width: a positive integer
-            height: a positive integer
-
-        Attributes:
-            __width: the width of the rectangle
-            __height: the height of the rectangle
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
