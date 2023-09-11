10f3c946aad7a07c454a6098c7f478d4e80a3435
omnia <omniannnnnnn@gmail.com>
2023-09-11 22:18:59 +0000

    task11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Contains the BaseGeometry, Rectangle and Square classes
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A subclass of Rectangle that represents a square"""
+
+    def __init__(self, size):
+        """Initializes a Square instance
+
+        Args:
+            size: a positive integer
+
+        Attributes:
+            __size: the size of the square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Returns a string representation of the square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
