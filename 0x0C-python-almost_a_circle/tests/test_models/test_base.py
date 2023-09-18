0734fc56fb525e8ca0b356289f5bcbadcf1381b1
omnia <omniannnnnnn@gmail.com>
2023-09-18 16:13:34 +0000

    tasks

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,26 +1,17 @@
 import unittest
 from models.base import Base
+"""Test_Base"""
 
-class TestBase(unittest.TestCase):
-    """Class TestBase"""
+
+class test_Base(unittest.TestCase):
+    """Class test_Base"""
 
     def test_init(self):
-        # Test the id value for the first instance of Base
         a = Base()
         self.assertEqual(a.id, 1)
 
-        # Test the id value for the second instance of Base
         a1 = Base()
         self.assertEqual(a1.id, 2)
 
-        # Test the id value for the third instance of Base
-        a2 = Base()
-        self.assertEqual(a2.id, 3)
-    
-        # Test the id value for the fourth instance of Base with a specific value
-        a3 = Base(12)
-        self.assertEqual(a3.id, 12)
-
-        # Test the id value for the fifth instance of Base
-        a4 = Base()
-        self.assertEqual(a4.id, 4)
+        a2 = Base(12)
+        self.assertEqual(a2.id, 12)
