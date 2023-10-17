b940cfc1605cedb7b1006dad13410323a96799a6
OMNIA93 <omniannnnnnn@gmail.com>
2023-10-17 06:55:40 -0700

    test

--- 102-top_city.sql
+++ 102-top_city.sql
@@ -1,7 +1,3 @@
--- Display the top 3 of cities temperature during July and August ordered by temperature
-SELECT city, AVG(value) AS avg_temp
-FROM temperatures
-WHERE month IN (7, 8)
-GROUP BY city
-ORDER BY avg_temp DESC
-LIMIT 3;
+-- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
+-- displays the top 3 values of a column in a table
+SELECT `city`, AVG(`value`) 'avg_temp' FROM `temperatures` WHERE `month` = 7 OR `month` = 8 GROUP BY `city` ORDER BY `avg_temp` DESC LIMIT 3;
