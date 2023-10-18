1d0c4f53bc6cd964c970bc7c8c716f3cf7294f5b
OMNIA93 <omniannnnnnn@gmail.com>
2023-10-18 16:01:22 -0700

    task3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,15 +1,3 @@
-#!/bin/bash
-
-# Check if the table force_name already exists
-if [[ $(mysql -u your_username -p -e "USE $1; DESCRIBE force_name;" 2>/dev/null) ]]; then
-  echo "The table force_name already exists in the database $1."
-else
-  # Create the table if it doesn't exist
-  mysql -u your_username -p $1 <<EOF
-  CREATE TABLE force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL
-  );
-EOF
-  echo "Table force_name has been created in the database $1."
-fi
+-- creates the table force_name on your MySQL server
+-- creates table with id and name param in db
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
