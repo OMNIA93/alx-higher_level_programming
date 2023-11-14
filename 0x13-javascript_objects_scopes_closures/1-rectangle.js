e4ae98117a37f626df0f9d939fc5eeeda9f987b2
OMNIA93 <omniannnnnnn@gmail.com>
2023-11-14 13:11:19 -0800

    task1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
-constructor (w,h){
+  constructor (w, h) {
     this.width = w;
-    this.hight = h;
-    }
+    this.height = h;
+  }
 }
 module.exports = Rectangle;
