f4d23b611e5751a850be0e1f2ced06cc1455afc0
OMNIA93 <omniannnnnnn@gmail.com>
2023-11-19 15:43:34 -0800

    tyk

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""Prints all City objects from the database hbtn_0e_14_usa"""
+"""class definition of a State"""
 
 import sys
-from model_city import City
+from model_city import Base, City
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(bind=engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
+    result = session.query(City, State).order_by(City.id).filter(
+        City.state_id == State.id).all()
 
-    result = session.query(City, State).join(State, City.state_id == State.id)\
-                    .order_by(City.id)
-    for city, state in result:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    for row in result:
+        print("{}: ({}) {}".format(row.State.name, row.City.id, row.City.name))
